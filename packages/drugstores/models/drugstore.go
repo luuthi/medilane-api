467079a8624eabdd5c2e364d1337f2f2e3c834b7
hoangnd2 <hoangnd2@f99.com.vn>
2021-05-19 22:48:56 +0700

    update model

--- packages/drugstores/models/drugstore.go
+++ packages/drugstores/models/drugstore.go
@@ -14,4 +14,13 @@ type DrugStore struct {
 	CommonModelFields
 
 	StoreName    string `json:"store_name" gorm:"type:varchar(200);not null"`
+	PhoneNumber string `json:"phone_number" gorm:"type:varchar(200)"`
+	Manager string `json:"manager" gorm:"type:varchar(200)"`
+	TaxNumber string `json:"tax_number" gorm:"type:varchar(200)"`
+	LicenseFile string `json:"license_file" gorm:"type:varchar(200)"`
+	Status string `json:"status" gorm:"type:varchar(200)"`
+	CaringStaff string `json:"caring_staff" gorm:"type:varchar(200)"`
+	Type string `json:"type" gorm:"type:varchar(200)"`
+	ApproveTime time.Time `json:"approve_time"`
+	ApproveBy string `json:"approve_by" gorm:"type:varchar(200)"`
 }
