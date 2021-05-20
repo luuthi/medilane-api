a4faf5c857a8e669b2c174c229c92702bc31ca32
hoangnd2 <hoangnd2@f99.com.vn>
2021-05-20 23:04:34 +0700

    update model

--- db/seeders/drugstore_sedder.go
+++ db/seeders/drugstore_sedder.go
@@ -1,10 +1,6 @@
 package seeders
 
 import (
-	"golang.org/x/crypto/bcrypt"
-	"log"
-	models2 "medilane-api/packages/accounts/models"
-
 	"github.com/jinzhu/gorm"
 )
 
@@ -17,40 +13,18 @@ func NewDrugStoreSeeder(db *gorm.DB) *DrugStoreSeeder {
 }
 
 func (drugstoreSeeder *DrugStoreSeeder) SetDrugStores() {
-	drugstores := map[int]map[string]interface{}{
-		1: {
-			"StoreName":     "thild@gmail.com",
-		},
-		2: {
-			"StoreName":     "admin@gmail.com",
-		},
-	}
-
-	if !drugstoreSeeder.DB.HasTable(&models2.User{}) {
-		drugstoreSeeder.DB.CreateTable(&models2.User{})
-
-		for key, value := range drugstores {
-			user := models2.User{}
-			drugstoreSeeder.DB.First(&user, key)
-			encryptedPassword, err := bcrypt.GenerateFromPassword(
-				[]byte(value["password"].(string)),
-				bcrypt.DefaultCost,
-			)
-			if err != nil {
-				log.Fatal("Error hash password")
-			}
-
-			if user.ID == 0 {
-				user.ID = uint(key)
-				user.Email = value["email"].(string)
-				user.Username = value["username"].(string)
-				user.FullName = value["full_name"].(string)
-				user.Password = string(encryptedPassword)
-				user.IsAdmin = value["is_admin"].(bool)
-				user.Type = value["type"].(string)
-				user.Status = value["status"].(bool)
-				userSeeder.DB.Create(&user)
-			}
-		}
-	}
+	//drugstores := map[int]map[string]interface{}{
+	//	1: {
+	//		"StoreName":     "thild@gmail.com",
+	//	},
+	//	2: {
+	//		"StoreName":     "admin@gmail.com",
+	//	},
+	//}
+	//
+	//if !drugstoreSeeder.DB.HasTable(&models2.User{}) {
+	//	drugstoreSeeder.DB.CreateTable(&models2.User{})
+	//
+	//
+	//}
 }
--- packages/drugstores/models/drugstore.go
+++ packages/drugstores/models/drugstore.go
@@ -24,3 +24,57 @@ type DrugStore struct {
 	ApproveTime time.Time `json:"approve_time"`
 	ApproveBy string `json:"approve_by" gorm:"type:varchar(200)"`
 }
+
+type DrugStoreAccount struct {
+	Relationship string `json:"phone_number" gorm:"type:varchar(200)"`
+	DrugStore DrugStore `gorm:"foreignKey:DrugStore.ID"`
+}
+
+type DrugStoreRelationship struct {
+	ChildStore DrugStore `gorm:"foreignKey:DrugStore.ID"`
+	ParentStore DrugStore `gorm:"foreignKey:DrugStore.ID"`
+}
+
+type DrugStoreProduct struct {
+	CommonModelFields
+
+	Quantity    int `json:"quantity" gorm:"type:integer(8);not null"`
+}
+
+type Consignment struct {
+	CommonModelFields
+
+	Code    string `json:"code" gorm:"type:varchar(200);not null"`
+}
+
+type DrugStoreConsignment struct {
+	CommonModelFields
+
+	Quantity    int `json:"quantity" gorm:"type:integer(8);not null"`
+	Consignment Consignment `gorm:"foreignKey:Consignment.ID"`
+	DrugStoreProduct DrugStoreProduct `gorm:"foreignKey:DrugStoreProduct.ID"`
+}
+
+type DeliveryReceiptBill struct {
+	CommonModelFields
+
+	OrderCode    string `json:"order-code" gorm:"type:varchar(200);not null"`
+	Type    string `json:"type" gorm:"type:varchar(200)"`
+	Discount	float32  `json:"discount" gorm:"type:float(8)"`
+	SubTotal	float32  `json:"sub-total" gorm:"type:float(8)"`
+	Total	float32  `json:"total" gorm:"type:float(8)"`
+	Vat	float32  `json:"vat" gorm:"type:float(8)"`
+	Note    string `json:"note" gorm:"type:varchar(200)"`
+	Status    string `json:"status" gorm:"type:varchar(200)"`
+	DrugStore DrugStore `gorm:"foreignKey:DrugStore.ID"`
+}
+
+type DeliveryReceiptBillDetail struct {
+	CommonModelFields
+
+	Cost	float32  `json:"cost" gorm:"type:float(8)"`
+	Quantity    int `json:"quantity" gorm:"type:integer(8);not null"`
+	Discount	float32  `json:"discount" gorm:"type:float(8)"`
+}
+
+
